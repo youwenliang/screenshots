a78f75093159c0194606e92c7301715e8a0a9c64
Jared Hirsch <ohai@6a68.net>
2017-04-06 11:32:02 -0700

    Nit - replace let..in with let..of

--- addon/webextension/background/analytics.js
+++ addon/webextension/background/analytics.js
@@ -42,8 +42,8 @@ var analytics = (function () {
       options.applicationName = di.appName;
       options.applicationVersion = di.version;
       let abTests = auth.getAbTests();
-      for (let testName in abTests) {
-        options[abTests[testName].gaField] = abTests[testName].value;
+      for (let [gaField, value] of Object.entries(abTests)) {
+        options[gaField] = value;
       }
       dump(`sendEvent ${eventCategory}/${action}/${label || 'none'} ${JSON.stringify(options)}`);
       req.send(JSON.stringify({
